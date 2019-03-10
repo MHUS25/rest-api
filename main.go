7f760c790f2c3bda9410513e573b9b6f0d2fce2c
Muna <mmhussein125@gmail.com>
2019-03-10 01:45:52 +0000

    creates Book struct

--- main.go
+++ main.go
@@ -7,6 +7,13 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
 func main() {
 	router := mux.NewRouter()
 
