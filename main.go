80e852bf2c40c91af6ba3157cd3b0b48ec3b1f20
Muna <mmhussein125@gmail.com>
2019-03-10 01:36:09 +0000

    creates route handlers and endpoints

--- main.go
+++ main.go
@@ -6,4 +6,10 @@ import (
 
 func main() {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/api/books", getBooks).Methods("GET")
+	router.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/api/books", createBook).Methods("POST")
+	router.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 }
